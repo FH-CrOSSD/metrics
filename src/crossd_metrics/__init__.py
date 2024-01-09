9eb2ceae3d233b05643e78d54a648aec89d7666d
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-01-09 15:18:28 +0100

    Revert "bugfix for crawling"
    This reverts commit cca39450673d2afb0bc0f6cc431f1c2a71c5a944.

--- src/crossd_metrics/__init__.py
+++ src/crossd_metrics/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding=utf-8 -*-
-__all__ = ["Repository", "User", "RepositoryOwner", "Organiuation"]
+__all__ = ['Repository', 'User', 'RepositoryOwner', 'Organiuation']
 import os
-import base64
 from pathlib import Path
 
 import crossd_metrics
@@ -20,32 +19,31 @@ transport = RequestsHTTPTransport(
     retries=3,
     timeout=100,
     headers={
-        "Authorization": f'bearer {os.environ.get("GH_TOKEN").strip()}',
-        "Accept": "application/vnd.github.hawkgirl-preview+json",
-    },
-)
+        'Authorization': f'bearer {os.environ.get("GH_TOKEN").strip()}',
+        'Accept': 'application/vnd.github.hawkgirl-preview+json'
+    })
 
 client = Client(
     transport=transport,
     execute_timeout=100,
     # fetch_schema_from_transport=True,
     schema=open(
-        Path(crossd_metrics.__file__).parent.joinpath("schema.docs.graphql")
-    ).read(),
-)
+        Path(crossd_metrics.__file__).parent.joinpath(
+            "schema.docs.graphql")).read())
 
 ds = DSLSchema(client.schema)
+
 
 
 # Authentication is defined via github.Auth
 
 # using an access token
-auth = Auth.Token(base64.b64decode(os.environ.get("GH_TOKEN").strip()))
+auth = Auth.Token(os.environ.get("GH_TOKEN").strip())
 
 # First create a Github instance:
 
 # Public Web Github
-gh = Github(auth=auth, per_page=100)
+gh = Github(auth=auth,per_page=100)
 
 # # Then play with your Github objects:
 # for repo in g.get_user().get_repos():
